dca2c68f9dd5726a42b41d989846a950baff2285
shanshkelqeje <118238788+shanshkelqeje@users.noreply.github.com>
2023-10-04 02:56:00 +0800

    valid mountain array

---
+++ Arrays 101/4-searching-for-items-in-an-array/valid-mountain-array.js
@@ -0,0 +1,36 @@
+/**
+ * @param {number[]} arr
+ * @return {boolean}
+ */
+var validMountainArray = function(arr) {
+    if(arr.length < 3) { // Mountain array must have array length >= 3
+        return false;
+    }
+    ascendingMountain = 1;
+    descendingMountain = 0;
+    for(var i = 0; i < arr.length; i++) {
+        if(arr[i] > arr[i+1] && ascendingMountain) {
+            descendingMountain = 1;
+            ascendingMountain = 0;
+        }
+        else if(arr[i] == arr[i+1]) {
+            return false;
+        }
+        if(i == 0 && descendingMountain) {
+            return false;
+        }
+        if(arr[i] <= arr[i+1] && descendingMountain) {
+            return false;
+        }
+    }
+    if(descendingMountain == 0) { // Array is strictly increasing, not a mountain array
+        return false;
+    }
+    else {
+        return true;
+    }
+};
+
+arr = [9,8,7,6,5,4,3,2,1,0];
+
+console.log(validMountainArray(arr));
