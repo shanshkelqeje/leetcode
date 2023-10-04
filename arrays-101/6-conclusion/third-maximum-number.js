56a79f6cd595ac49acdc6e5c36fc0080981894bc
shanshkelqeje <118238788+shanshkelqeje@users.noreply.github.com>
2023-10-05 00:06:44 +0800

    third maximum number wip

--- arrays-101/6-conclusion/third-maximum-number.js
+++ arrays-101/6-conclusion/third-maximum-number.js
@@ -3,8 +3,31 @@
  * @return {number}
  */
 var thirdMax = function(nums) {
-    
+    distinctNos = [];
+    maximumNo = 0;
+    for(var i = 0; i < nums.length; i++) {
+        if(distinctNos.length < 3) {
+            distinctNos.push(nums[i]);
+        }
+        for(var j = 0; j < distinctNos.length; j++) {
+            if(distinctNos[j] == distinctNos[j+1]) {
+                distinctNos.splice(j+1, 1);
+            }
+        }
+    }
+    console.log(distinctNos);
+    if(distinctNos.length == 3) {
+        return distinctNos[2];
+    }
+    else {
+        for(num of distinctNos) {
+            if(maximumNo < num) {
+                maximumNo = num;
+            }
+        }
+        return maximumNo;
+    }
 };
 
-nums = [3,2,1];
+nums = [1,2,2,5,3,5];
 console.log(thirdMax(nums));
