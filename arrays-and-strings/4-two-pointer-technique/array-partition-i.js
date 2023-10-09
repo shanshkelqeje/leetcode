db66e5286d01ea821a6a8b17b19041232eb53657
shanshkelqeje <118238788+shanshkelqeje@users.noreply.github.com>
2023-10-09 16:12:01 +0800

    update array-partition-i.js

--- arrays-and-strings/4-two-pointer-technique/array-partition-i.js
+++ arrays-and-strings/4-two-pointer-technique/array-partition-i.js
@@ -6,8 +6,8 @@ var arrayPairSum = function(nums) {
     totalSum = 0;
     // Sort array into non-decreasing order
     nums.sort(function(a, b){return a - b});
-    // Two-pointer technique to itereate through array
-    // Reason: Smallest num should be paired with largest num to return maximised result
+    // Iterate through array
+    // Reason: largest num should be paired with largest num and smallest num should be paired with smallest num to return maximised result
     i = 0;
     for(var j = 0; j < nums.length; j++) {
         totalSum += Math.min(nums[j], nums[j+1]);
