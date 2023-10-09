6a9c10fd75bd7a3e959ba99edfd8dc88777d8077
shanshkelqeje <118238788+shanshkelqeje@users.noreply.github.com>
2023-10-09 18:26:45 +0800

    feat: create minimum-size-subarray-sum.js (wip)

---
+++ arrays-and-strings/4-two-pointer-technique/minimum-size-subarray-sum.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} target
+ * @param {number[]} nums
+ * @return {number}
+ */
+var minSubArrayLen = function(target, nums) {
+    // Using two-pointer
+    left = 0;
+    right = nums.length;
+    
+};
+
+target = 15;
+nums = [1, 2, 3, 4, 5];
+
+console.log(minSubArrayLen(target,nums));
