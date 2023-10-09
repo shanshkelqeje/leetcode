2723ad9493957947bf46410bba1aa21c60b592ef
shanshkelqeje <118238788+shanshkelqeje@users.noreply.github.com>
2023-10-09 15:19:56 +0800

    update 14.longest-common-prefix.js

--- 14.longest-common-prefix.js
+++ 14.longest-common-prefix.js
@@ -11,12 +11,19 @@
  */
 var longestCommonPrefix = function(strs) {
     commonPrefix = '';
-    for(var i = 0; i < strs.length; i++) {
+    previousPrefix = '';
+    for(var i = 0; i < strs[0].length; i++) {
+        commonPrefix = strs[0].slice(0, i + 1);
         for(var j = 0; j < strs.length; j++) {
-            
+            if(strs[j].startsWith(commonPrefix)) {
+                continue;
+            }
+            else {
+                return previousPrefix;
+            }
         }
+        previousPrefix = commonPrefix;
     }
+    return previousPrefix;
 };
-strs = ["flower","flow","flight"];
-console.log(longestCommonPrefix(strs));
 // @lc code=end
