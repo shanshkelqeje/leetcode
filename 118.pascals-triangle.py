f301e10fa10340c063ad5a9216ecd10d54c2bc7c
shanshkelqeje <118238788+shanshkelqeje@users.noreply.github.com>
2023-10-16 15:48:45 +0800

    feat: create 118.pascals-triangle.py (wip)

---
+++ 118.pascals-triangle.py
@@ -0,0 +1,11 @@
+#
+# @lc app=leetcode id=118 lang=python3
+#
+# [118] Pascal's Triangle
+#
+
+# @lc code=start
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        
+# @lc code=end
