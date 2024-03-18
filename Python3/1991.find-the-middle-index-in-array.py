7aea007eb027ecb7998f3008ea41e4c0176c655c
shanshkelqeje <118238788+shanshkelqeje@users.noreply.github.com>
2024-03-19 01:16:51 +0800

    Create find-the-middle-index-in-array.py

---
+++ Python3/1991.find-the-middle-index-in-array.py
@@ -0,0 +1,12 @@
+#
+# @lc app=leetcode id=1991 lang=python3
+#
+# [1991] Find the Middle Index in Array
+#
+
+# @lc code=start
+class Solution:
+    def findMiddleIndex(self, nums: List[int]) -> int:
+        
+# @lc code=end
+
