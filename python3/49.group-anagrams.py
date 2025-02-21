ff43fb7cfa0c439b35920062153a9bc0d22c4481
shanshkelqeje <118238788+shanshkelqeje@users.noreply.github.com>
2025-02-21 15:57:41 +0800

    create: 49.group-anagrams.py

---
+++ python3/49.group-anagrams.py
@@ -0,0 +1,19 @@
+#
+# @lc app=leetcode id=49 lang=python3
+#
+# [49] Group Anagrams
+#
+
+# @lc code=start
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        result = defaultdict(list)
+        for string in strs:
+            count = [0] * 26
+            for char in string:
+                count[ord(char) - ord('a')] += 1
+            result[tuple(count)].append(string)
+        return list(result.values())
+                
+# @lc code=end
+
