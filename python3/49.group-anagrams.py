7c50cb2edafa37f0dcca050e3bb7a72f4a810b0e
shanshkelqeje <118238788+shanshkelqeje@users.noreply.github.com>
2025-02-21 16:39:54 +0800

    update: 49.group-anagrams.py

--- python3/49.group-anagrams.py
+++ python3/49.group-anagrams.py
@@ -7,11 +7,11 @@
 # @lc code=start
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        result = defaultdict(list)
+        result = defaultdict(list) # Key: Frequency count of characters / Value: List of anagrams
         for string in strs:
             count = [0] * 26
             for char in string:
-                count[ord(char) - ord('a')] += 1
+                count[ord(char) - ord('a')] += 1 # ASCII values
             result[tuple(count)].append(string)
         return list(result.values())
                 
